5c8307fdf948f6fedff289c60002d485a3d03f03
Chad Richard <chad.richard@gmail.com>
2016-08-28 09:45:14 -0400

    updated dotfiles for Fedora 24 env

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
-source ~/dotfiles/git/detailed-git-prompt.sh
-source ~/dotfiles/git/completion.bash
+#source ~/dotfiles/git/detailed-git-prompt.sh
+#source ~/dotfiles/git/completion.bash
+
+if [ -f `which powerline-daemon` ]; then
+    powerline-daemon -q
+      POWERLINE_BASH_CONTINUATION=1
+        POWERLINE_BASH_SELECT=1
+          . /usr/share/powerline/bash/powerline.sh
+fi
 
