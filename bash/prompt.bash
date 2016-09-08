a892a340f5d5807e7d0bda9c64bf6dc56a0c170f
Chad Richard <chad.richard@gmail.com>
2016-09-07 21:31:03 -0400

    dot file updates for a ubuntu build

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -3,10 +3,8 @@
 #source ~/dotfiles/git/detailed-git-prompt.sh
 #source ~/dotfiles/git/completion.bash
 
-if [ -f `which powerline-daemon` ]; then
-    powerline-daemon -q
-      POWERLINE_BASH_CONTINUATION=1
-        POWERLINE_BASH_SELECT=1
-          . /usr/share/powerline/bash/powerline.sh
+#powerline
+if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
+  source /usr/share/powerline/bindings/bash/powerline.sh
 fi
 
